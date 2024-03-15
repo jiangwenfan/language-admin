0aafc27e94098574f5c9609e7b189f8c386fd8ec
evans.jiang <evans.jiang@gmail.com>
2024-03-15 17:48:26 +0800

    init:最新

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # language-admin
 language的后台管理系统
+- [] docker compose 部署: https://github.com/docker/awesome-compose/tree/master/react-nginx
---
+++ start_loacal_dev.sh
@@ -0,0 +1,10 @@
+# 开始本地开发
+
+# create container
+docker compose up -d --build
+
+# open vscode
+code .
+
+# vscode settings
+echo -e "\nopen a remote window \n--> attach to Running Container \n--> choice own container "
